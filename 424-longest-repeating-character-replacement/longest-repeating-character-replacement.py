86e7f62c875fca1238926758700fee07ce036342
Aashik Sandeep <aashiksandeep4@gmail.com>
2026-07-31 15:35:16 +0530

    Time: 199 ms (12.09%) | Memory: 19.6 MB (50.39%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #use frequency array to identify which character is most frequent
+        #window to be invalid -> window - max(freq) > k
+        longest = 0
+        i = 0
+        freq = [0] * 26
+
+        for j in range(len(s)):
+            freq[ord(s[j]) - ord('A')] += 1
+            while ((j - i) + 1) - max(freq) > k:
+                freq[ord(s[i]) - ord('A')] -= 1
+                i += 1
+            longest = max(longest, (j - i) + 1)    
+           
+        return longest
