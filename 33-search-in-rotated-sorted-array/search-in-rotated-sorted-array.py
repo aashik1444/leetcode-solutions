721ad15b9ee0e472461523cb0f4cffb5f3c65be3
Aashik Sandeep <aashiksandeep4@gmail.com>
2026-08-01 11:21:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (77.19%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -8,7 +8,7 @@ class Solution:
                 l = m + 1
             else:
                 r = m
-        
+
         min_i = l
 
         if min_i == 0:
@@ -20,11 +20,11 @@ class Solution:
 
         while l <= r:
             m = (l + r) // 2
-            if nums[m] == target:
+            if target == nums[m]:
                 return m
-            elif nums[m] < target:
+            elif target > nums[m]:
                 l = m + 1
             else:
                 r = m - 1
-
+        
         return -1
